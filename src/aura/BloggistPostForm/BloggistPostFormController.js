45e4e926706722f6c9aac7bf7741b35e9e96ea2a
Alvaro C <acarneiro@altimetrik.com>
2018-05-21 14:41:31 -0300

    Working on post creation: Writer/author and post state

--- src/aura/BloggistPostForm/BloggistPostFormController.js
+++ src/aura/BloggistPostForm/BloggistPostFormController.js
@@ -1,4 +1,8 @@
 ({
+	initialize: function(component, event, helper) {
+		helper.loadStatuses(component);
+		helper.loadContacts(component);
+	},
 	onTagsUpdated : function(component, event, helper) {
 
 		const post = JSON.parse(JSON.stringify(component.get('v.post')));
@@ -8,5 +12,10 @@
 		post.Tags__c = tags.join(',');
 
 		component.set('v.post', post);
+	},
+
+	savePost: function(component, event, helper) {
+		console.log('Save!');
+		helper.savePost(component, event);
 	}
 })
--- src/aura/BloggistPostForm/BloggistPostFormHelper.js
+++ src/aura/BloggistPostForm/BloggistPostFormHelper.js
@@ -1,5 +1,50 @@
 ({
-	logState : function(component) {
-		
+	loadStatuses: function(component) {
+		const api = component.get('c.getPostStates');
+
+		api.setCallback(this, response => {
+			const state = response.getState();
+
+			if (state === 'SUCCESS') {
+				const options = response.getReturnValue();
+				component.set('v.options', options);
+			}
+		});
+
+		$A.enqueueAction(api);
+	},
+	loadContacts: function(component) {
+		const api = component.get('c.getContacts');
+
+		api.setCallback(this, response => {
+			const state = response.getState();
+
+			if (state === 'SUCCESS') {
+				const options = response.getReturnValue();
+				component.set('v.contacts', options);
+			}
+		});
+
+		$A.enqueueAction(api);
+	},
+	savePost: function(component, event) {
+		const api = component.get('c.savePost');
+
+		const post = JSON.parse(JSON.stringify(component.get('v.post')));
+
+		api.setParam('post', post);
+
+		api.setCallback(this, response => {
+			const state = response.getstate();
+			console.log('Yeah');
+
+			if (state === 'SUCCESS') {
+				const post = response.getReturnValue();
+				component.set('v.post', post);
+			}
+
+		});
+
+		$A.enqueueAction(api);
 	}
 })
